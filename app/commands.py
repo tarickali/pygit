3bc5158dade48f4273318515c68ad314425042b9
tarickali <tarickali97@gmail.com>
2025-01-06 11:59:36 -0500

    Read a blob object

--- app/commands.py
+++ app/commands.py
@@ -1,4 +1,7 @@
+from typing import Annotated
 import os
+import zlib
+from pathlib import Path
 
 import typer
 
@@ -17,3 +20,16 @@ def init():
         f.write("ref: refs/heads/main\n")
 
     print("Initialized git directory")
+
+
+@app.command()
+def cat_file(
+    blob_hash: Annotated[str, typer.Argument()],
+    pretty: Annotated[bool, typer.Option("-p")] = True,
+):
+    blob_path = Path(f".git/objects/{blob_hash[:2]}/{blob_hash[2:]}")
+    with open(blob_path, "br") as f:
+        blob = zlib.decompress(f.read())
+        _, content = blob.split(b"\x00")
+
+    print(content.decode(), end="")
